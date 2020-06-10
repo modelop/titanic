70402ec01bbf1231e06b20802ba2e228d32ad50f
Steve Avsec <stephen.avsec@opendatagroup.com>
2020-06-10 09:44:31 -0500

    Fixed up to have the training function run

--- titanic.py
+++ titanic.py
@@ -1,4 +1,7 @@
+#fastscore.recordsets.0: true
+
 import pandas as pd
+import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -7,15 +10,22 @@ from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 
-
+#modelop.init
 def begin():
     global model
-    model = pickle.load(open('model.pkl', 'rb'))
+    try:
+        model = pickle.load(open('model.pkl', 'rb'))
+    except FileNotFoundError:
+        pass
 
-
-
+#modelop.train
 def train(train_df):
 
+    numeric_columns = ['PassengerId', 'Survived', 'Pclass','Age', 'SibSp', 
+                       'Parch', 'Fare']
+    train_df.replace(to_replace=[None], value=np.nan, inplace=True)
+    train_df[numeric_columns] = train_df.loc[:, numeric_columns] \
+            .apply(pd.to_numeric, errors='coerce')
     X_train = train_df.drop('Survived', axis=1)
     y_train = train_df['Survived']
 
@@ -46,16 +56,19 @@ def train(train_df):
     model.fit(X_train, y_train)
 
     #where do we write this?? s3?  fixed location on contaier?  or get location from env variable?
-    pickle.dump(model, open('model.pkl', 'wb'))
+    with open('model.pkl', 'wb') as f:
+        pickle.dump(model, f)
 
+    pass
 
+#modelop.metrics
 def metrics(df):
 
     X_test = df.drop('Survived', axis=1)
     y_test = df['Survived']
     yield { "ACCURACY": model.score(X_test, y_test)}
 
-
+#modelop.score
 def predict(X):
     df = pd.DataFrame(X, index=[0])
     y_pred = model.predict(df)
