3e65833b337b675001a3980f6663c316e0387dcf
benmackenzie-exos <ben.mackenzie@exosfinancial.com>
2020-08-28 09:38:43 -0400

    undo some of Steve's  code

--- titanic.py
+++ titanic.py
@@ -17,30 +17,31 @@ def begin():
 
 #modelop.train
 def train(train_df):
-    numeric_columns = ['PassengerId', 'Survived', 'Pclass','Age', 'SibSp',
-                       'Parch', 'Fare']
-    train_df.replace(to_replace=[None], value=np.nan, inplace=True)
-    train_df[numeric_columns] = train_df.loc[:, numeric_columns] \
-            .apply(pd.to_numeric, errors='coerce')
     X_train = train_df.drop('Survived', axis=1)
     y_train = train_df['Survived']
+
     numeric_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='mean')),
         ('scaler', StandardScaler())])
+
     categorical_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
         ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
     numeric_features = X_train.select_dtypes(include=['int64', 'float64']).columns
+
     categorical_features = X_train.select_dtypes(include=['object']).columns
+
     preprocessor = ColumnTransformer(
         transformers=[
             ('num', numeric_transformer, numeric_features),
             ('cat', categorical_transformer, categorical_features)])
-    model = Pipeline(steps=[('preprocessor', preprocessor),
-                          ('classifier', RandomForestClassifier())])
-    model.fit(X_train, y_train)
 
-    #where do we write this?? s3?  fixed location on contaier?  or get location from env variable?
+    model = Pipeline(steps=[('preprocessor', preprocessor),
+                            ('classifier', RandomForestClassifier())])
+
+    model.fit(X_train, y_train)
+    
     with open('outputDir/model.pkl', 'wb') as f:
         pickle.dump(model, f)
     pass
