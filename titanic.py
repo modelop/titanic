054c856ad693376b6c6a19d431dde69e61934a71
merhi-odg <51339977+merhi-odg@users.noreply.github.com>
2021-03-21 13:19:51 -0500

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -17,6 +17,9 @@ def begin():
 
 #modelop.train
 def train(train_df):
+    
+    print("Beginning Tarining: \n", flush=True)
+    
     X_train = train_df.drop('Survived', axis=1)
     y_train = train_df['Survived']
 
@@ -42,30 +45,35 @@ def train(train_df):
 
     model.fit(X_train, y_train)
     
+    print("Finished Tarining.\n", flush=True)
+    
+    print("Saving trained model: \n", flush=True)
+    
     with open('outputDir/model.pkl', 'wb') as f:
         pickle.dump(model, f)
+        
+    print("Finished saving trained model.\n", flush=True)
+
     pass
 
 #modelop.metrics
 def metrics(df):
 
+    print("Beginning Metrics: \n", flush=True)
+
     X_test = df.drop('Survived', axis=1)
     y_test = df['Survived']
+    
+    print("Yielding Metrics: \n", flush=True)
+
     yield { "ACCURACY": model.score(X_test, y_test)}
 
 #modelop.score
 def predict(X):
+    
+    print("Beginning Scoring: \n", flush=True)
+
     df = pd.DataFrame(X, index=[0])
     y_pred = model.predict(df)
     for p in y_pred:
         yield p
-
-
-
-
-
-
-
-
-
-
