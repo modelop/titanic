a477ca94076ea9baa4da831498b2b754cee135ed
merhi-odg <sami.merhi@opendatagroup.com>
2021-08-30 10:11:00 -0400

    remove recordsets from slot 1

--- titanic.py
+++ titanic.py
@@ -1,7 +1,6 @@
 # modelop.schema.0: input_schema.avsc
 # modelop.schema.1: output_schema.avsc
 # modelop.recordsets.0: true
-# modelop.recordsets.1: true
 
 import pandas
 import numpy
@@ -41,7 +40,7 @@ def predict(scoring_df):
         scoring_df[numeric_predictors + categorical_predictors]
     )
     # The smart-comment # modelop.recordsets.1: true yields a DataFrame as JSON-lines
-    yield scoring_df
+    yield scoring_df.to_dict(orient="records")
 
 
 # modelop.metrics
